3698d082e6a07fce75b5e1d2834ed9334d525270
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-23 10:14:12 -0300

    Add: Ask a question page and bootstrap

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 
 // Defines EJS as HTML renderer
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get("/", (req,res) => {
-    res.render('index')
+    res.render('index');
 });
 
-app.get("/user", (req,res) => {
-    res.render('profile/index')
+app.get("/ask", (req,res) => {
+    res.render('ask');
 });
-
 
 app.listen(3000, () => {
     console.log('App running on port: 3000')
