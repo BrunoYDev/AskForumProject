e00db63ae21aef551e9b1ccb48eb9dba961b7d9d
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-23 12:12:00 -0300

    Add: Questions model and ask page

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
 const express = require("express");
 const app = express();
-const connection = require('./database/database')
+const connection = require("./database/database");
+const questionModel = require("./database/Question");
+const Question = require("./database/Question");
+const { Order } = require("sequelize");
 
-connection.authenticate().then(() => {
-    console.log('connection stable')
-})
-.catch((errorMsg) => {
+connection
+  .authenticate()
+  .then(() => {
+    console.log("connection stable");
+  })
+  .catch((errorMsg) => {
     console.log(errorMsg);
-})
+  });
 
 // Defines EJS as HTML renderer
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  Question.findAll({ raw: true, order: [["id", "desc"]] }).then((questions) => {
+    res.render("index", {
+      questions: questions,
+    });
+  });
 });
 
-app.get("/ask", (req,res) => {
-    res.render('ask');
+app.get("/ask", (req, res) => {
+  res.render("ask");
 });
 
-app.post("/savequestion", (req,res) => {
-    let title = req.body.title;
-    let description = req.body.description;
-    res.send(`Form Received! Title: ${title} Description: ${description}`);
+app.post("/savequestion", (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  Question.create({
+    title: title,
+    description: description,
+  }).then(() => {
+    res.redirect("/");
+  });
 });
 
 app.listen(3000, () => {
-    console.log('App running on port: 3000')
+  console.log("App running on port: 3000");
 });
