5afac5c27f39d2529cf234b72008aa00abf8a68b
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-23 11:41:42 -0300

    Add: Connection with DB

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const connection = require('./database/database')
+
+connection.authenticate().then(() => {
+    console.log('connection stable')
+})
+.catch((errorMsg) => {
+    console.log(errorMsg);
+})
 
 // Defines EJS as HTML renderer
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.render('index');
@@ -11,6 +22,12 @@ app.get("/", (req,res) => {
 
 app.get("/ask", (req,res) => {
     res.render('ask');
+});
+
+app.post("/savequestion", (req,res) => {
+    let title = req.body.title;
+    let description = req.body.description;
+    res.send(`Form Received! Title: ${title} Description: ${description}`);
 });
 
 app.listen(3000, () => {
